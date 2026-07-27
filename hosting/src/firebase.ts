9ce7d395c56a0e464afd350cd2f0da0c13f73591
BRUNOGOMESTIECIA <bg@tiecia.com.br>
2026-07-27 15:24:50 -0300

    correção chave

--- hosting/src/firebase.ts
+++ hosting/src/firebase.ts
@@ -10,12 +10,12 @@ import { getFirestore } from 'firebase/firestore';
  */
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZyrcBfFRjhhMOpkhxLLrzgZ5vII6Tl98",
-  authDomain: "instapasso.firebaseapp.com",
-  projectId: "instapasso",
-  storageBucket: "instapasso.firebasestorage.app",
-  messagingSenderId: "190667143384",
-  appId: "1:190667143384:web:7b97b8b82d7912dfd2bfad"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 /** @description Instância principal do aplicativo Firebase */
